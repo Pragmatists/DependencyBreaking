878a790fd626f7b22ead24b182ff4a3cc175373f
Michael Bareja <michal.bareja@pragmatists.pl>
2016-09-15 12:31:55 +0200

    added empty test

---
+++ src/test/java/tdd/dependencyBreaking/cart/CartControllerTest.java
@@ -0,0 +1,13 @@
+package tdd.dependencyBreaking.cart;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
+public class CartControllerTest {
+
+    @Test
+    public void anEmptyTest() throws Exception {
+        assertThat(true).isTrue();
+    }
+}
